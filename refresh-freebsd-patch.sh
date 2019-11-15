89bec4e0ab40e9268f7109f92b15a3394bc2d378
AJ Jordan <alex@strugee.net>
2019-11-14 22:49:13 -0500

    Use bash for refresh-freebsd-patch.sh
    I think it makes brace expansion work and I don't really care to debug.
    We already require bash for the test suite anyway so who cares really.

--- refresh-freebsd-patch.sh
+++ refresh-freebsd-patch.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 # This is useful to do because it suppresses messages about individual hunks
 cp filter-other-days filter-other-days.freebsd && patch filter-other-days.freebsd freebsd-bug-237752-hack.patch && diff -u filter-other-days{,.freebsd} > freebsd-bug-237752-hack.patch
